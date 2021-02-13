16001a0b900545c44ef93e20970540f91c046f89
RyanKeel <ryanlkeel@gmail.com>
2021-02-13 17:04:44 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_tutorial_w_dart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_tutorial_w_dart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter_tutorial_w_dart/team.dart';
+import 'package:flutter_tutorial_w_dart/teamlist.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This is the root widget of the application.
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      theme: ThemeData(
+
+      )
+    )
+  }
+}
